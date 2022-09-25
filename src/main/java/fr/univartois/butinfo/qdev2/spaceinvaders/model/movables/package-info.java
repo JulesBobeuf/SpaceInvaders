4b7d6933e8fba8186174fef864fd08fe63e3f63c
Romain Wallon <romain.wallon@univ-artois.fr>
2022-09-25 11:04:09 +0200

    :sparkles: Ajoute une implémentation de base pour les objets IMovable.

---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Le paquetage {@code fr.univartois.butinfo.qdev2.spaceinvaders.model.movables} fournit
+ * des implantations pour différents objets pouvant se déplacer dans le jeu du
+ * Space-Invaders.
+ *
+ * @author Romain Wallon
+ *
+ * @version 0.1.0
+ */
+
+package fr.univartois.butinfo.qdev2.spaceinvaders.model.movables;
