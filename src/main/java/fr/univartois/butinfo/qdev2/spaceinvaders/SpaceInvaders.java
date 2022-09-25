d6c7340db53d6c7bbe16582c97f7518175468bf2
Romain Wallon <romain.wallon@univ-artois.fr>
2022-09-25 11:05:55 +0200

    :sparkles: Ajoute la classe principale de l'application.

---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/SpaceInvaders.java
@@ -0,0 +1,89 @@
+/**
+ * Ce logiciel est distribué à des fins éducatives.
+ *
+ * Il est fourni "tel quel", sans garantie d’aucune sorte, explicite
+ * ou implicite, notamment sans garantie de qualité marchande, d’adéquation
+ * à un usage particulier et d’absence de contrefaçon.
+ * En aucun cas, les auteurs ou titulaires du droit d’auteur ne seront
+ * responsables de tout dommage, réclamation ou autre responsabilité, que ce
+ * soit dans le cadre d’un contrat, d’un délit ou autre, en provenance de,
+ * consécutif à ou en relation avec le logiciel ou son utilisation, ou avec
+ * d’autres éléments du logiciel.
+ *
+ * (c) 2022 Romain Wallon - Université d'Artois.
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.qdev2.spaceinvaders;
+
+import java.io.IOException;
+
+import fr.univartois.butinfo.qdev2.spaceinvaders.controller.SpaceInvadersController;
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.SpaceInvadersGame;
+import fr.univartois.butinfo.qdev2.spaceinvaders.view.SpriteStore;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * La classe SpaceInvaders permet de lancer l'implantation en JavaFX du jeu
+ * Space-Invaders.
+ *
+ * @author Romain Wallon
+ *
+ * @version 0.1.0
+ */
+public final class SpaceInvaders extends Application {
+
+    /**
+     * La largeur (en pixels) de la fenêtre affichant le jeu.
+     */
+    private static final int GAME_WIDTH = 1080;
+
+    /**
+     * La hauteur (en pixels) de la fenêtre affichant le jeu.
+     */
+    private static final int GAME_HEIGHT = 720;
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see javafx.application.Application#start(javafx.stage.Stage)
+     */
+    @Override
+    public void start(Stage stage) throws IOException {
+        // On commence par charger la vue et son contrôleur.
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view/space-invaders.fxml"));
+        Parent viewContent = fxmlLoader.load();
+        SpaceInvadersController controller = fxmlLoader.getController();
+        controller.setStage(stage);
+
+        // On crée ensuite le jeu, que l'on lie au contrôleur.
+        SpaceInvadersGame game = new SpaceInvadersGame(
+                GAME_WIDTH, GAME_HEIGHT, new SpriteStore());
+        controller.setGame(game);
+        game.setController(controller);
+        game.prepare();
+
+        // On peut maintenant afficher la scène et la fenêtre.
+        Scene scene = new Scene(viewContent, GAME_WIDTH, GAME_HEIGHT);
+        stage.setScene(scene);
+        stage.setTitle("Space Invaders");
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    /**
+     * Exécute l'application JavaFX du jeu Space-Invaders.
+     *
+     * @param args Les arguments de la ligne de commande (dont on ne tient pas compte).
+     *
+     * @see #launch(String...)
+     */
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Le paquetage {@code fr.univartois.butinfo.qdev2.spaceinvaders} fournit une implantation
+ * en JavaFX du jeu Space-Invaders.
+ *
+ * @author Romain Wallon
+ *
+ * @version 0.1.0
+ */
+
+package fr.univartois.butinfo.qdev2.spaceinvaders;
