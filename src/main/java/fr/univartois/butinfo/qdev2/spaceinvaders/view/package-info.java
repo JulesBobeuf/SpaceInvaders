1f9a587ece7641525d49e92eba1519d1014719f7
Romain Wallon <romain.wallon@univ-artois.fr>
2022-09-25 10:48:52 +0200

    :sparkles: Ajoute le code permettant de charger les images.

---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/view/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Le paquetage {@code fr.univartois.butinfo.qdev2.spaceinvaders.view} fournit les classes
+ * permettant de gérer la vue du jeu du Space-Invaders.
+ *
+ * @author Romain Wallon
+ *
+ * @version 0.1.0
+ */
+
+package fr.univartois.butinfo.qdev2.spaceinvaders.view;
