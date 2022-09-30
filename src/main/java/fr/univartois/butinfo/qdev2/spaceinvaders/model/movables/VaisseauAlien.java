7a3d36839dd94b191a3f4ea80b4af28be5485f44
Santoro Thomas <thomas_santoro@ens.univ-artois.fr>
2022-09-30 13:40:55 +0200

    fix #4

---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/VaisseauAlien.java
@@ -0,0 +1,72 @@
+/**
+ * Ce fichier fait partie du projet projet-2022-2023-b-1.
+ *
+ * (c) 2022 thomas.santoro
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.qdev2.spaceinvaders.model.movables;
+
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable;
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.SpaceInvadersGame;
+import fr.univartois.butinfo.qdev2.spaceinvaders.view.Sprite;
+
+/**
+ * Le type VaisseauAlien
+ *
+ * @author thomas.santoro
+ *
+ * @version 0.1.0
+ */
+public class VaisseauAlien extends AbstractMovable {
+
+    /**
+     * Crée une nouvelle instance de VaisseauAlien.
+     * @param game
+     * @param xPosition
+     * @param yPosition
+     * @param sprite
+     */
+    protected VaisseauAlien(SpaceInvadersGame game, double xPosition, double yPosition,Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+        this.setHorizontalSpeed(75.00);
+        this.setVerticalSpeed(1.00);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#move(long)
+     */
+    @Override
+    public boolean move(long delta) {
+        boolean x = super.move(delta);
+        if (x == false)
+            if (this.getY()==game.getBottomLimit()) {
+                game.alienReachedPlanet();
+                return false;
+            }
+            if (this.getX()==game.getLeftLimit()) {
+                this.setHorizontalSpeed(-(getHorizontalSpeed()*1.02));
+                return false;
+            }
+            if (this.getX()==game.getRightLimit()) {
+                this.setHorizontalSpeed(-(getHorizontalSpeed()*1.02));
+                return false;
+            }
+        return true;
+    }
+    
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#collidedWith(fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable)
+     */
+    @Override
+    public void collidedWith(IMovable other) {
+        // TODO Auto-generated method stub.
+    }
+    
+
+}
+
