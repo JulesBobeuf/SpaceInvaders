561760b174b449e6d7db0ca4dca56a782c80200d
JAKOBOWSKI Aymeric <aymeric_jakobowski@ens.univ-artois.fr>
2022-09-30 13:04:49 +0200

    Fix #3 et modif settings.gradle

---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/VaisseauJoueur.java
@@ -0,0 +1,49 @@
+/**
+ * Ce fichier fait partie du projet projet-2022-2023-b-1.
+ *
+ * (c) 2022 aymeric.jakobowski
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.qdev2.spaceinvaders.model.movables;
+
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable;
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.SpaceInvadersGame;
+import fr.univartois.butinfo.qdev2.spaceinvaders.view.Sprite;
+
+/**
+ * Le type VaisseauJoueur
+ *
+ * @author aymeric.jakobowski
+ *
+ * @version 0.1.0
+ */
+public class VaisseauJoueur extends AbstractMovable {
+
+    /**
+     * Crée une nouvelle instance de VaisseauJoueur.
+     * 
+     * @param game
+     * @param xPosition
+     * @param yPosition
+     * @param sprite
+     */
+    protected VaisseauJoueur(SpaceInvadersGame game, double xPosition, double yPosition,
+            Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+        this.setHorizontalSpeed(150.00);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#collidedWith(fr.univartois
+     * .butinfo.qdev2.spaceinvaders.model.IMovable)
+     */
+    @Override
+    public void collidedWith(IMovable other) {
+        //TODO membre 1
+    }
+
+}
