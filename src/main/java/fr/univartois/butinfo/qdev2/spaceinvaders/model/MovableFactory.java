ca4485449c30e05c9d387408769302e01d5d1577
Pierre Schreiner <pierre.schreiner@info0728.ipa.iutlens.local>
2022-09-30 13:54:03 +0200

    Implémentation de l'interface IMovableFactory

---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/MovableFactory.java
@@ -0,0 +1,87 @@
+/**
+ * Ce fichier fait partie du projet projet-2022-2023-b-1.
+ *
+ * (c) 2022 pierre.schreiner
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.qdev2.spaceinvaders.model;
+
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.Tir;
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.VaisseauJoueur;
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.VaisseauAlien;
+import fr.univartois.butinfo.qdev2.spaceinvaders.view.ISpriteStore;
+
+/**
+ * Le type MovableFactory
+ *
+ * @author pierre.schreiner
+ *
+ * @version 0.1.0
+ */
+public class MovableFactory implements IMovableFactory {
+
+    private ISpriteStore spriteStore;
+
+    private SpaceInvadersGame game;
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovableFactory#setSpriteStore(fr.
+     * univartois.butinfo.qdev2.spaceinvaders.view.ISpriteStore)
+     */
+    @Override
+    public void setSpriteStore(ISpriteStore spriteStore) {
+        this.spriteStore = spriteStore;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovableFactory#setGame(fr.
+     * univartois.butinfo.qdev2.spaceinvaders.model.SpaceInvadersGame)
+     */
+    @Override
+    public void setGame(SpaceInvadersGame game) {
+        this.game = game;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovableFactory#createAlien(int,
+     * int)
+     */
+    @Override
+    public IMovable createAlien(int x, int y) {
+        return new VaisseauAlien(game, x, y, spriteStore.getSprite("ship"));
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovableFactory#createShip(int,
+     * int)
+     */
+    @Override
+    public IMovable createShip(int x, int y) {
+        return new VaisseauJoueur(game, x, y, spriteStore.getSprite("ship"));
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovableFactory#createShot(int,
+     * int)
+     */
+    @Override
+    public IMovable createShot(int x, int y) {
+        return new Tir(game, x, y, spriteStore.getSprite("shot"));
+    }
+
+}
