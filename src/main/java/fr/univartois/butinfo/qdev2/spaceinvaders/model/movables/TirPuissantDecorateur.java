b417ed83fe39b6f9b4d90c386fbcb8e7c194bfe1
Jules B <bobeuf.jules@gmail.com>
2022-10-07 14:07:17 +0200

    t

---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/TirPuissantDecorateur.java
@@ -0,0 +1,23 @@
+/**
+ * Ce fichier fait partie du projet projet-2022-2023-b-1.
+ *
+ * (c) 2022 Jules
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.qdev2.spaceinvaders.model.movables;
+
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable;
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.SpaceInvadersGame;
+import fr.univartois.butinfo.qdev2.spaceinvaders.view.Sprite;
+
+/**
+ * Le type TirPuissantDecorateur
+ *
+ * @author Jules
+ *
+ * @version 0.1.0
+ */
+public class TirPuissantDecorateur { // implement le truc de pierre
+}
+
