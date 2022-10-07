f61bcc5b8c4bbec488e10dc9c6de6744462de0cd
Pierre Schreiner <pierre.schreiner@info0624.ipa.iutlens.local>
2022-10-07 13:57:30 +0200

    +AbstractMovableDecorateur

---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/AbstractMovableDecorateur.java
@@ -0,0 +1,254 @@
+/**
+ * Ce fichier fait partie du projet projet-2022-2023-b-1.
+ *
+ * (c) 2022 pierre.schreiner
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.qdev2.spaceinvaders.model.movables;
+
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable;
+import fr.univartois.butinfo.qdev2.spaceinvaders.view.Sprite;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.DoubleProperty;
+
+/**
+ * Le type AbstractMovablePlusResistant
+ *
+ * @author pierre.schreiner
+ *
+ * @version 0.1.0
+ */
+public abstract class AbstractMovableDecorateur implements IMovable {
+    
+    private IMovable movable;
+
+    /**
+     * Crée une nouvelle instance de AbstractMovablePlusResistant.
+     * @param movable
+     */
+    public AbstractMovableDecorateur(IMovable movable) {
+        this.movable = movable;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#getWidth()
+     */
+    @Override
+    public int getWidth() {
+        return movable.getWidth();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#getHeight()
+     */
+    @Override
+    public int getHeight() {
+        return movable.getHeight();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#setX(int)
+     */
+    @Override
+    public void setX(int xPosition) {
+        movable.setX(xPosition);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#getX()
+     */
+    @Override
+    public int getX() {
+        return movable.getX();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#getXProperty()
+     */
+    @Override
+    public DoubleProperty getXProperty() {
+        return movable.getXProperty();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#setY(int)
+     */
+    @Override
+    public void setY(int yPosition) {
+        movable.setY(yPosition);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#getY()
+     */
+    @Override
+    public int getY() {
+        return movable.getY();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#getYProperty()
+     */
+    @Override
+    public DoubleProperty getYProperty() {
+        return movable.getYProperty();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#consume()
+     */
+    @Override
+    public void consume() {
+        movable.consume();        
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#isConsumed()
+     */
+    @Override
+    public boolean isConsumed() {
+        return movable.isConsumed();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#isConsumedProperty()
+     */
+    @Override
+    public BooleanProperty isConsumedProperty() {
+        return movable.isConsumedProperty();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#setHorizontalSpeed(double)
+     */
+    @Override
+    public void setHorizontalSpeed(double speed) {
+        movable.setHorizontalSpeed(speed);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#getHorizontalSpeed()
+     */
+    @Override
+    public double getHorizontalSpeed() {
+        return movable.getHorizontalSpeed();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#setVerticalSpeed(double)
+     */
+    @Override
+    public void setVerticalSpeed(double speed) {
+        movable.setVerticalSpeed(speed);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#getVerticalSpeed()
+     */
+    @Override
+    public double getVerticalSpeed() {
+        return movable.getVerticalSpeed();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#move(long)
+     */
+    @Override
+    public boolean move(long timeDelta) {
+        return movable.move(timeDelta);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#getSprite()
+     */
+    @Override
+    public Sprite getSprite() {
+        return movable.getSprite();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#isCollidingWith(fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable)
+     */
+    @Override
+    public boolean isCollidingWith(IMovable other) {
+        return movable.isCollidingWith(other);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#collidedWith(fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable)
+     */
+    @Override
+    public void collidedWith(IMovable other) {
+        movable.collidedWith(other);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#collidedWith(fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.Tir)
+     */
+    @Override
+    public void collidedWith(Tir other) {
+        movable.collidedWith(other);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#collidedWith(fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.VaisseauAlien)
+     */
+    @Override
+    public void collidedWith(VaisseauAlien other) {
+        movable.collidedWith(other);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovable#collidedWith(fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.VaisseauJoueur)
+     */
+    @Override
+    public void collidedWith(VaisseauJoueur other) {
+        movable.collidedWith(other);
+    }    
+}
+
